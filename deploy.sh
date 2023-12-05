d5dc2015a1995d1a7207b9f09ab0213a949bec61
PeterWang-dev <PeterWang030908@hotmail.com>
2023-12-05 22:40:40 +0800

    feat: basic structure of deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BASE_DIR=$(pwd)/word_book
+
+# Check installation of Docker and docker-compose
+
+# Permission check
+
+# Get presquite files
+
+# Run docker-compose
+docker-compose up -d
